1aab90f8efe9d0dcf462dc11ce8c8ea1ce6a6e15
Blai <blaimunyoz@gmail.com>
2020-05-16 10:10:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+lista = [345,'hola',0,'mas cosas']
+
+diccionario = {'dato1':456,'hola':'error23','mas cosas':4}
+
+print('hello world!')
+
+for i in diccionario:
+    print('key', i)
+    print('valor', diccionario[i])
+
+print('Fin programa')
